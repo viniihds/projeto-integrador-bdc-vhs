4b8eb1549dbc07c97935ce53e1870aee45bc603d
Renata Quintela <vihquintela0604@gmail.com>
2022-05-23 14:22:49 -0300

    ok

--- projeto-integrador/src/components/SidenavComp.vue
+++ projeto-integrador/src/components/SidenavComp.vue
@@ -10,19 +10,19 @@ export default {};
       <a href=""><button class="btn-sidenav">Ver mais</button></a>
     </ul>
     <div class="sidenav-times">
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/assoeva logo.png" alt=""></a>
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/jaragua logo.png" alt=""></a>
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/taubate.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://upload.wikimedia.org/wikipedia/pt/6/6d/Assoeva_Futsal.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://ligafutsal.com.br/wp-content/uploads/2016/06/escudo-PNG-Jaragu%C3%A1.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://static.wixstatic.com/media/3b007e_973e67f395cd4f33921df430c3be4486~mv2.png/v1/fill/w_151,h_152,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/3b007e_973e67f395cd4f33921df430c3be4486~mv2.png" alt=""></a>
     </div>
     <div class="sidenav-times">
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/praia clube logo.png" alt=""></a>
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/corinthians logo.jpg" alt=""></a>
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/tubarao logo.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://4.bp.blogspot.com/-7RVUKEyg-iM/XLTNoZgA6VI/AAAAAAAAXbc/b57mTjl-5hoSpTU4aFUYjjljhrAHOjJhwCLcBGAs/s1600/PCU.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://logodownload.org/wp-content/uploads/2016/11/corinthians-logo-0.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://ligafutsal.com.br/wp-content/uploads/2016/06/Escudo-Tubar%C3%A3o-02.png" alt=""></a>
     </div>
     <div class="sidenav-times">
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/magnus logo.png" alt=""></a>
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/carlos barbosa logo.png" alt=""></a>
-      <a href=""><img class="sidenav-imgs" src="../assets/imgs/jec logo.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://magnusfutsal.com.br/images/logo-magnusfutsal.png?v1" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://ligafutsal.com.br/wp-content/uploads/2016/06/Escudo-ACBF.png" alt=""></a>
+      <a href=""><img class="sidenav-imgs" src="https://www.jecfutsal.com.br/wp-content/uploads/sites/709/2020/02/escudo-Jec-Futsal-bco.png" alt=""></a>
     </div>
     <a href=""><button class="btn-sidenav">Ver mais</button></a>
   </aside>
